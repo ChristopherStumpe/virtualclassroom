acababf9f4502ea7c20d4dad4fedc735f4a7c8bb
Bri-b <bri.skinner02@gmail.com>
2020-10-02 21:27:21 -0500

    (chore) connect passport.js and Oauth 2.0
    ordered the files so that passport works correctly, added passport routers

--- server/index.js
+++ server/index.js
@@ -1,23 +1,56 @@
 /* eslint-disable no-console */
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-
 const app = express();
-// const { teacherRouter } = require('./routes/student');
-// const { studentRouter } = require('./routes/teacher');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./auth/passport.setup');
+const {isLoggedIn} = require('./auth/verifyLogIn');
+const cors = require('cors');
+require('dotenv').config();
 
-require('./db/db');
+// Cookies and Session info
+app.use(cookieSession({
+  name: 'user',
+  keys: ['key1']
+}));
 
-const PORT = process.env.SERVER_PORT || 8080;
-
-// Middleware
+// middleware
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use(express.json());
+
+// Routers
+// const { teacherRouter } = require('./routes/student');
+// const { studentRouter } = require('./routes/teacher');
+const passportRouter = require('./auth/routes');
+
+// Database
+// require('./db/db');
+
+const PORT = process.env.SERVER_PORT || 8080;
 
 // Routes //
 // app.use('/teacher', teacherRouter);
 // app.use('/student', studentRouter);
+app.use('/auth/google', passportRouter);
+
+app.get('/', (req, res) => {
+  res.send("you are not logged in")
+})
+
+app.get('/account', isLoggedIn, (req, res) => {
+  res.send("you are logged in")
+})
+
+app.get('/logout', (req, res) => {
+  // console.log(`Serving ${req.method} at ${req.url}`);
+  req.session = null; //destory the session
+  req.logOut(); // logout from passport
+  // console.log(`cookie ${req.cookies}`) // the cookie should have the entire user info
+  // console.log(`session ${req.session}`) // the cookie should have the entire user info
+  res.redirect('/'); // send them to where is needed
+})
 
 app.listen(PORT, () => {
   console.log(`🌌Server has started on port: 🚀${PORT}`);
