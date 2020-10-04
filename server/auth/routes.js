1e0978284e596731704f8bb1f897eb3800fd4c3c
Bri-b <bri.skinner02@gmail.com>
2020-10-04 12:31:16 -0500

    (cleanup) add pseudocode

--- server/auth/routes.js
+++ server/auth/routes.js
@@ -1,12 +1,20 @@
-const passport = require('passport');
-const express = require('express');
+const passport = require("passport");
+const express = require("express");
 const router = express.Router();
 
-router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  "/",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
-router.get('/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-  // Successful authentication, redirect home.
-  res.redirect('/account');
-});
+router.get(
+  "/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Unsuccessful authentication redirect home.
+    // Successful authentication redirect to account
+    res.redirect("/account");
+  }
+);
 
 module.exports = router;
