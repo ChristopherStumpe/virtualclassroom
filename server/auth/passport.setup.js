3eae5766b9a08be9712ac2bbf605d60d90cbb8d1
Bri-b <bri.skinner02@gmail.com>
2020-10-04 12:29:09 -0500

    (cleanup) remove console logs

--- server/auth/passport.setup.js
+++ server/auth/passport.setup.js
@@ -4,13 +4,12 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 require("dotenv").config();
 
 passport.serializeUser(function (user, done) {
-  // console.log("!!!seralize!!", user)
+  // Setting user obj on req.session
   done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-  // find user in db and entire user
-  let log = 0;
+  // present user obj to use to find things in the db
   const data = {
     id: user.id,
     fullName: user.displayName,
@@ -19,9 +18,6 @@ passport.deserializeUser(function (user, done) {
     email: user.emails[0].value,
     photo: user.photos[0].value,
   };
-  console.log(log === 0 ? "logged in" : "logged out");
-  log === 0 ? (log = 1) : (log = 0);
-  // console.log("!!!de-seralize!!", data);
   done(null, user);
 });
 
@@ -34,9 +30,7 @@ passport.use(
       callbackURL: `http://localhost:${process.env.SERVER_PORT}/auth/google/callback`,
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token: ", accessToken, "refresh token", refreshToken);
       // accessToken, and refreshToken can be used for additional google products
-      // use the profile info (profile id) to check if the user is registered in the db
       const data = {
         id: profile.id,
         fullName: profile.displayName,
@@ -45,9 +39,9 @@ passport.use(
         email: profile.emails[0].value,
         photo: profile.photos[0].value,
       };
+      // use the profile info (profile id) to check if the user is registered in the db
       // if the user doesn't exists, save to db, else selected the user and pass them to the done function
       // signal that this is done
-      console.log("!!! instance of google profile!!", data);
       return done(null, profile);
     }
   )
