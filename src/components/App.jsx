fe2adf3c40e176a6dd803832ceacd0584b7abab0
Bri-b <bri.skinner02@gmail.com>
2020-10-05 15:32:24 -0500

    (chore) add first component
    App is the log in wire frame

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [account, setCount] = useState("logged out");
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You are ${account}`;
+  });
+
+  return (
+    <div>
+      <p>You are {account}</p>
+      <button
+        onClick={() =>
+          account === "logged out"
+            ? setCount("logged in")
+            : setCount("logged out")
+        }
+      >
+        click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
