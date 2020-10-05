a17035fc5a76032d654ad7a659ba25f7d3590703
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-05 15:46:02 -0500

    (chore) student route

---
+++ server/routes/student.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const { Router } = require('express');
+const sequelize = require('sequelize');
+const {
+  Announcement, Announcement_class, Assignment, Assignment_class, Assignment_student, Class, School, Student, Student_class, Teacher,
+} = require('../db/db');
+
+const studentRouter = Router();
