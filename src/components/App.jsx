a7806e9113a7465555829d14a1235ee48bf8e70f
Bri-b <bri.skinner02@gmail.com>
2020-10-06 12:25:11 -0500

    (chore) change view
    views are teacher, student, login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import TeachSplashScreen from './TeachSplashScreen';
+import StudSplashScreen from './StudSplashScreen';
 
-function App() {
-  const [isOnline, setIsOnline] = useState('logged out');
-  useEffect(() => {
-    document.title = `You are ${isOnline}`;
-  });
-
-  // Similar to componentDidMount and componentDidUpdate:
-
+export default function App() {
+  const [user, setUser] = useState(null);
+  const viewRender = (e) => {
+    if (e.target.name === 'teacher') {
+      setUser(e.target.name);
+      console.log(e.target.name);
+    } else if (e.target.name === 'student') {
+      setUser(e.target.name);
+      console.log(e.target.name);
+    } else if (e.target.name === 'logout') {
+      setUser(null);
+      console.log(e.target.name);
+    }
+  };
+  // on login click
+  // direct users to a page based off of the type of user they are
+  // check if they are a teacher or student
+  // if teacher render teach splash screen
+  // if student render student splash screen
+  // redirect to log in and alert you are not a authorized _(fill in role)_
+  // currently state changes to is logged in and is logged out on click
   return (
     <div>
-      <p>
-        You are
-        {isOnline}
-      </p>
       <button
+        name="teacher"
         type="button"
-        onClick={() => (isOnline === 'logged out'
-          ? setIsOnline('logged in')
-          : setIsOnline('logged out'))}
+        onClick={viewRender}
       >
-        click me
+        teacher login
       </button>
+      <button
+        name="student"
+        type="button"
+        onClick={viewRender}
+      >
+        student login
+      </button>
+      <button
+        name="logout"
+        type="button"
+        onClick={viewRender}
+      >
+        logout
+      </button>
+      <div>
+        {user === 'teacher' && <TeachSplashScreen />}
+        {user === 'student' && <StudSplashScreen />}
+      </div>
     </div>
   );
 }
-
-export default App;
