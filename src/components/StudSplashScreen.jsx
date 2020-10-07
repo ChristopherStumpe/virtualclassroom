32088acbb6d8fa212770fd20dbadc3f152610d79
Bri-b <bri.skinner02@gmail.com>
2020-10-07 15:32:32 -0500

    (chore) make view switch more dynamic

--- src/components/StudSplashScreen.jsx
+++ src/components/StudSplashScreen.jsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import AnnouncementList from './allUsers/AnnouncementList';
 import AssignmentList from './allUsers/AssignmentList';
 import ClassList from './allUsers/ClassList';
 
-export default function StudSplashScreen({ name }) {
-  // const user = useState('student');
+export default function StudSplashScreen({ name, user }) {
   return (
     <div>
       <h1>
@@ -14,15 +13,17 @@ export default function StudSplashScreen({ name }) {
         {name}
       </h1>
       <AnnouncementList />
-      <AssignmentList user="student" />
+      <AssignmentList user={user} />
       <ClassList />
     </div>
   );
 }
 StudSplashScreen.propTypes = {
   name: PropTypes.string,
+  user: PropTypes.string,
 };
 
 StudSplashScreen.defaultProps = {
   name: '',
+  user: '',
 };
