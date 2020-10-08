36f7d8f4e1352dc3dd9041c58890b2efa83bc4bc
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-07 20:26:05 -0500

    (fix) Announcement Model

--- server/db/models/Announcement.js
+++ server/db/models/Announcement.js
@@ -1,17 +1,31 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('announcement', {
-  announcement_title: {
-    type: DataTypes.STRING,
-  },
-  description: {
-    type: DataTypes.STRING,
-  },
-  release_time: {
-    type: DataTypes.STRING,
-  },
-  expiration_date: {
-    type: DataTypes.STRING,
-  },
-}, {
-  freezeTableName: true,
-  timestamps: true,
-});
+module.exports = (sequelize, DataTypes) => {
+  const Announcement = sequelize.define('announcement', {
+    announcement_title: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    release_time: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    expiration_date: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+  }, {
+    freezeTableName: true,
+  });
+
+  Announcement.associate = (models) => {
+    Announcement.belongsToMany(models.Class, {
+      through: 'announcement_class',
+      foreignKey: 'id_announcement',
+    });
+  };
+
+  return Announcement;
+};
