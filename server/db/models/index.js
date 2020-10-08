d24ef5a0f592f4181c2600512565fb5cb6520ee7
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-08 02:36:59 -0500

    (update) index file to reflect new models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -15,13 +15,16 @@ const sequelize = new Sequelize(
   },
 );
 
-const AnnouncementModel = require('./Announcement')
-const AssignmentModel = require('./Assignment')
-const ClassModel = require('./Class')
-const SchoolModel = require('./School')
-const StudentModel = require('./Student')
-const TeacherModel = require('./Teacher')
-const Assignment_studentModel = require('./Assignment_student')
+const AnnouncementModel = require('./Announcement');
+const AssignmentModel = require('./Assignment');
+const ClassModel = require('./Class');
+const SchoolModel = require('./School');
+const StudentModel = require('./Student');
+const TeacherModel = require('./Teacher');
+const Assignment_studentModel = require('./Assignment_student');
+const Student_classModel = require('./Student_class');
+const Assignment_classModel = require('./Assignment_class');
+const Announcement_classModel = require('./Announcement_class');
 
 const Announcement = AnnouncementModel(sequelize, Sequelize);
 const Assignment = AssignmentModel(sequelize, Sequelize);
@@ -30,6 +33,9 @@ const School = SchoolModel(sequelize, Sequelize);
 const Student = StudentModel(sequelize, Sequelize);
 const Teacher = TeacherModel(sequelize, Sequelize);
 const Assignment_student = Assignment_studentModel(sequelize, Sequelize);
+const Student_class = Student_classModel(sequelize, Sequelize);
+const Assignment_class = Assignment_classModel(sequelize, Sequelize);
+const Announcement_class = Announcement_classModel(sequelize, Sequelize);
 
 const models = {
   Announcement,
@@ -39,6 +45,9 @@ const models = {
   Student,
   Teacher,
   Assignment_student,
+  Student_class,
+  Assignment_class,
+  Announcement_class,
 };
 
 Object.keys(models).forEach((model) => {
