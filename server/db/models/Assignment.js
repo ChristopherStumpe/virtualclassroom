4fd73054f233934a72f06e44d6ecd68a91ac38f5
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-07 20:31:00 -0500

    (fix) Assignment model

--- server/db/models/Assignment.js
+++ server/db/models/Assignment.js
@@ -1,17 +1,31 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('assignment', {
-  assignment_name: {
-    type: DataTypes.STRING,
-  },
-  description: {
-    type: DataTypes.STRING,
-  },
-  due_date: {
-    type: DataTypes.TIME,
-  },
-  release_time: {
-    type: DataTypes.STRING,
-  },
-}, {
-  freezeTableName: true,
-  timestamps: true,
-});
+module.exports = (sequelize, DataTypes) => {
+  const Assignment = sequelize.define('assignment', {
+    assignment_name: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    due_date: {
+      type: DataTypes.DATE,
+    },
+    release_time: {
+      type: DataTypes.STRING,
+    },
+  }, {
+    freezeTableName: true,
+  });
+
+  Assignment.associate = (models) => {
+    Assignment.belongsToMany(models.Student, {
+      through: models.Assignment_student,
+      foreignKey: 'id_assignment',
+    });
+    Assignment.belongsToMany(models.Class, {
+      through: 'assignment_class',
+      foreignKey: 'id_assignment',
+    });
+  };
+
+  return Assignment;
+};
