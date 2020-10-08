9bc72796832522464a9bf74e685782bc4b3facbe
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-07 20:33:55 -0500

    (fix) class model

--- server/db/models/Class.js
+++ server/db/models/Class.js
@@ -1,31 +1,40 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('class', {
-  class_name: {
-    type: DataTypes.STRING,
-  },
-  period: {
-    type: DataTypes.INTEGER,
-  },
-  start_time: {
-    type: DataTypes.TIME,
-  },
-  end_time: {
-    type: DataTypes.TIME,
-  },
-  id_school: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'school',
-      key: 'id',
+module.exports = (sequelize, DataTypes) => {
+  const Class = sequelize.define('class', {
+    class_name: {
+      type: DataTypes.STRING,
     },
-  },
-  id_teacher: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'teacher',
-      key: 'id',
+    period: {
+      type: DataTypes.INTEGER,
     },
-  },
-}, {
-  freezeTableName: true,
-  timestamps: true,
-});
+    start_time: {
+      type: DataTypes.TIME,
+    },
+    end_time: {
+      type: DataTypes.TIME,
+    },
+  }, {
+    freezeTableName: true,
+  });
+
+  Class.associate = (models) => {
+    Class.belongsToMany(models.Student, {
+      through: 'student_class',
+      foreignKey: 'id_class',
+    });
+    Class.belongsToMany(models.Announcement, {
+      through: 'announcement_class',
+      foreignKey: 'id_class',
+    });
+    Class.belongsToMany(models.Assignment, {
+      through: 'assignment_class',
+      foreignKey: 'id_class',
+    });
+    Class.belongsTo(models.School, {
+      foreignKey: 'id_school',
+    });
+    Class.belongsTo(models.Teacher, {
+      foreignKey: 'id_teacher',
+    });
+  };
+  return Class;
+};
