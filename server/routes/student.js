5c9c1e48fecbca1e394962957e8c8bc74e3b67b5
Christopher Stumpe <toph.stumpe@gmail.com>
2020-10-09 14:01:38 -0500

    (update) student routes

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,8 +1,30 @@
-/* eslint-disable no-console */
-const { Router } = require('express');
-const sequelize = require('sequelize');
-const {
-  Announcement, Announcement_class, Assignment, Assignment_class, Assignment_student, Class, School, Student, Student_class, Teacher,
-} = require('../db/db');
+// /* eslint-disable no-console */
+// const { Router } = require('express');
+// // const sequelize = require('sequelize');
+// const { models } = require('../db/models/index');
 
-const studentRouter = Router();
+// const {
+//   Announcement,
+//   Announcement_class,
+//   Assignment, Assignment_class,
+//   Assignment_student,
+//   Class,
+//   School,
+//   Student,
+//   Student_class,
+//   Teacher,
+//   sequelize,
+//   Sequelize } = models;
+// const studentRouter = Router();
+
+// module.exports = {
+//   studentRouter,
+// };
+
+//
+
+
+
+module.exports = {
+  userRouter,
+};
